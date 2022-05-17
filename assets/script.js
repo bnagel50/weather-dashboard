679dddc22fcbea7cc3522511d65d01ebecee82ff
bnagel <nagelb500@gmail.com>
2022-05-16 23:13:19 -0400

    added bulk of code for js

--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,26 @@ var weatherContainerEl = document.querySelector("#weather-container");
 var citySearchTerm = document.querySelector("#city-search-term");
 var APIKey = "862fdaa7f4383326d44318f27fde2561";
 
+var latitude;
+var longitude;
+var searchTerm;
+var currentWeather;
+var futureWeather;
+var date;
+var icon;
+var wind;
+var uvIndex;
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
 
-    var city = cityInputEl.value.trim();
     
+    var city = cityInputEl.value.trim();
+
     if (city) {
-        getWeather(city);
+        getCoords(city);
+        getWeather(latitude, longitude);
+        displayWeather(searchTerm, currentWeather, futureWeather, date, icon, wind, uvIndex);
         weatherContainerEl.textContent = '';
         cityInputEl.value = '';
     } else {
@@ -19,31 +31,57 @@ var formSubmitHandler = function(event) {
     }
 };
 
-var getWeather = function(city, APIKey) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
-    fetch(apiUrl)
-        .then(function(response) {
-            if (response.ok) {
-                console.log(response);
-                response.json().then(function(data) {
-                    console.log(data);
-                    displayWeather(data.items);
-                });
-            } else {
-                alert('Error ' + response.statusText);
-            }
+var getCoords = function(city) {
+    var coordApi = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + APIKey;
+    fetch(coordApi)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            var latitude = data[0].lat;
+            var longitude = data[0].lon;
+            getWeather(latitude, longitude);
         });
 };
 
-var displayWeather = function(searchTerm) {
+var getWeather = function(latitude, longitude) {
+    console.log(latitude);
+    console.log(longitude);
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey;
+    fetch(apiUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            var currentWeather = data.temp;
+            var futureWeather = data.daily;
+            var date = data.current.dt;
+            // var icon = data.weather.icon;
+            var wind = data.wind_speed;
+            var uvIndex = data.current.uvi;
+            console.log(data);
+            displayWeather(searchTerm, currentWeather, futureWeather, date, icon, wind, uvIndex);
+        });
+} 
+        
+    
+
+var displayWeather = function(searchTerm, currentWeather, futureWeather, date, icon, wind, uvIndex) {
     citySearchTerm.textContent = searchTerm;
 
     var weatherEl = document.createElement("a");
-    weatherEl.classList = "flex-row list-item align-center justify-space-between"
-
+    weatherEl.classList = "flex-row list-item align-center justify-space-between";
+    weatherEl.append(currentWeather);
+    weatherEl.append(futureWeather);
+    weatherEl.append(date);
+    weatherEl.append(icon);
+    weatherEl.append(wind);
+    weatherEl.append(uvIndex);
+    
     var titleEl = document.createElement("span");
-    titleEl.textContent = city;
+    titleEl.textContent = searchTerm;
 
     weatherEl.appendChild(titleEl);
 
